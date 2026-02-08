9ac52e513e3495aaadbd46ecf90d6b5dc5147e57
Whiskas <whiskaskorn@gmail.com>
2026-02-08 22:57:17 +0200

    added roles

--- models/user_model.js
+++ models/user_model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     email: {type:String,required:true,unique:true},
     username: {type: String,required: true,unique: true},
-    password: {type: String,required: true}
+    password: {type: String,required: true},
+    roles: {type: [String], default: ['User']}
 })
 
 module.exports = mongoose.model('User',userSchema)
